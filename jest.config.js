989a4828eabfc48cda71594fdf02061fd5f51312
Moras123 <diego.moron@ucb.edu.bo>
2024-11-11 23:56:45 -0400

    intento 4

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    transform: {
-      "^.+\\.[tj]sx?$": "babel-jest",
-    },
-    transformIgnorePatterns: [
-      "node_modules/(?!(axios)/)" // Ignora node_modules excepto axios
-    ],
-    moduleNameMapper: {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-      },
-  };
-  
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(axios)/)", // Permite que Jest transforme axios
+  ],
+};
